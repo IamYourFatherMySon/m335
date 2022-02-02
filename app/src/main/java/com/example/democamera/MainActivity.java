a7b0f481abe0a473f98dcb9be75c04ac446e6b94
Dyonisos Mpliamplias <dyonisos.mpliamplias@inventage.com>
2022-02-02 22:06:54 +0100

    Some things

--- app/src/main/java/com/example/democamera/MainActivity.java
+++ app/src/main/java/com/example/democamera/MainActivity.java
@@ -1,59 +1,73 @@
 package com.example.democamera;
 
-import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+import static android.provider.MediaStore.MediaColumns.DISPLAY_NAME;
+import static android.provider.MediaStore.MediaColumns.MIME_TYPE;
+import static android.provider.MediaStore.MediaColumns.RELATIVE_PATH;
+import static androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA;
+import static androidx.camera.lifecycle.ProcessCameraProvider.getInstance;
+import static com.example.democamera.R.id.fillEnd;
+import static com.example.democamera.R.id.image_capture_button;
+import static com.example.democamera.R.id.position;
+import static com.example.democamera.R.id.video_capture_button;
+import static com.example.democamera.R.id.viewFinder;
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.net.Uri;
-import android.os.Bundle;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Environment;
-import android.util.Log;
-import android.view.Surface;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import androidx.camera.core.Camera;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCapture.OnImageSavedCallback;
+import androidx.camera.core.ImageCapture.OutputFileOptions;
+import androidx.camera.core.ImageCapture.OutputFileOptions.Builder;
+import androidx.camera.core.ImageCapture.OutputFileResults;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import androidx.camera.core.*;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.video.*;
-import androidx.camera.video.VideoCapture;
-import androidx.core.content.PermissionChecker;
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.camera.view.PreviewView;
-
-import com.example.democamera.databinding.ActivityMainBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import android.provider.MediaStore;
 
-import android.content.ContentValues;
-import android.os.Build;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+/**
+ * Ich bin blöd.
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-  //  private ActivityMainBinding viewBinding;
+
+    public static final String TAKEN_PHOTO_KEY = "takenPhoto";
+
+    //  private ActivityMainBinding viewBinding;
     private ImageCapture imageCapture;
     private VideoCapture<Recorder> videoCapture;
     private Recording recording;
@@ -71,87 +85,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             ActivityCompat.requestPermissions(
                     this,
-                    new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    new String[]{CAMERA, RECORD_AUDIO, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE},
                     10
             );
         }
 
         // Set up the listeners for take photo and video capture buttons
-        ((Button) findViewById(R.id.image_capture_button)).setOnClickListener(this);
+        findViewById(image_capture_button).setOnClickListener(this);
         //viewBinding.videoCaptureButton.setOnClickListener(this);
 
-        cameraExecutor = Executors.newSingleThreadExecutor();
-
+        cameraExecutor = newSingleThreadExecutor();
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.image_capture_button:
-                //action
-                this.takePhoto();
-                break;
-
-            case R.id.video_capture_button:
-                //action
-                this.captureVideo();
-                break;
-        }
-    }
-
-    private void takePhoto() {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "NEW_IMAGE");
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
-
-        ImageCapture.OutputFileOptions options = new ImageCapture.OutputFileOptions.Builder(
-                getContentResolver(),
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues).build();
-
-
-      //  ImageCapture.OutputFileOptions outputFileOptions =
-      //          new ImageCapture.OutputFileOptions.Builder(new File("hallo.jpg")).build();
-        imageCapture.takePicture(options, cameraExecutor,
-                new ImageCapture.OnImageSavedCallback() {
-                    @Override
-                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
-                        // insert your code here.
-                        Log.d("dfsdf", "dfsd");
-                    }
-                    @Override
-                    public void onError(ImageCaptureException error) {
-                        // insert your code here.
-                        Log.d("dfsdf", "dfsd2");
-                    }
-                }
-        );
-        Intent intent = new Intent(this, Photo.class);
-        startActivity(intent);
-
-    }
-
-    private void captureVideo() {}
-
-    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-        cameraProvider.unbindAll();
-        Preview preview = new Preview.Builder()
-                .build();
-
-
-     /*   CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                .build();  */
-        preview.setSurfaceProvider(((PreviewView) findViewById(R.id.viewFinder)).getSurfaceProvider());
-        imageCapture =
-                new ImageCapture.Builder()
-                        .setTargetRotation(((PreviewView) findViewById(R.id.viewFinder)).getDisplay().getRotation())
-                        .build();
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, CameraSelector.DEFAULT_BACK_CAMERA, imageCapture, preview);
+    private boolean allPermissionsGranted() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                CAMERA
+        ) == PERMISSION_GRANTED;
     }
 
     private void startCamera() {
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = getInstance(this);
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
@@ -163,11 +117,83 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }, ContextCompat.getMainExecutor(this));
     }
 
-    private boolean allPermissionsGranted() {
-        return ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-        ) == PackageManager.PERMISSION_GRANTED;
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case image_capture_button:
+                //action
+                takePhoto();
+                break;
+
+            case video_capture_button:
+                //action
+                captureVideo();
+                break;
+        }
+    }
+
+    private void takePhoto() {
+        ContentValues contentValues = new ContentValues();
+        String fileName = "CameraX_" + LocalDateTime.now();
+        contentValues.put(DISPLAY_NAME, fileName);
+        contentValues.put(MIME_TYPE, "image/jpeg");
+        contentValues.put(RELATIVE_PATH, "Pictures/CameraX-Image");
+//        File mediaStorageDir = getExternalFilesDir(Environment.getExternalStorageDirectory());
+
+        try {
+            Files.walk(Paths.get(Environment.getExternalStorageDirectory().toURI())).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        OutputFileOptions options =
+                new Builder(getContentResolver(), EXTERNAL_CONTENT_URI, contentValues)
+                .build();
+
+        File file = new File(fileName);
+
+        imageCapture.takePicture(options, cameraExecutor,
+                new OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(OutputFileResults outputFileResults) {
+                        // insert your code here.
+                        try {
+                            Files.copy(Paths.get(outputFileResults.getSavedUri().toString()), Paths.get(getApplicationContext().getDataDir().toURI()));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        File file1 = new File(Objects.requireNonNull(outputFileResults.getSavedUri()).toString());
+                        Intent photoIntent = new Intent(MainActivity.this, Photo.class);
+                        photoIntent.putExtra(TAKEN_PHOTO_KEY, file1.toString());
+                        startActivity(photoIntent);
+                        Log.d("ajodsfoajdsfojhasojfojhfooasdijofsda", outputFileResults.getSavedUri().getPath());
+                    }
+
+                    @Override
+                    public void onError(ImageCaptureException error) {
+                        // insert your code here.
+                        Log.d("dfsdf", "dfsd2");
+                    }
+                }
+        );
+
+    }
+
+    private void captureVideo() {
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        Preview preview = new Preview.Builder()
+                .build();
+
+        preview.setSurfaceProvider(((PreviewView) findViewById(viewFinder)).getSurfaceProvider());
+        imageCapture =
+                new ImageCapture.Builder()
+                        .setTargetRotation(findViewById(viewFinder).getDisplay().getRotation())
+                        .build();
+        Camera camera = cameraProvider.bindToLifecycle(this, DEFAULT_BACK_CAMERA, imageCapture, preview);
     }
 
     @Override
@@ -175,4 +201,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
         cameraExecutor.shutdown();
     }
+
 }
--- app/src/main/java/com/example/democamera/Photo.java
+++ app/src/main/java/com/example/democamera/Photo.java
@@ -1,11 +1,18 @@
 package com.example.democamera;
 
+import static android.net.Uri.fromFile;
+import static com.example.democamera.MainActivity.TAKEN_PHOTO_KEY;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Photo extends AppCompatActivity {
 
@@ -13,5 +20,31 @@ public class Photo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String photoFileName = extras.getString(TAKEN_PHOTO_KEY);
+            ImageView imageView = findViewById(R.id.imageView);
+            File file = new File(photoFileName);
+            System.out.println(file.exists());
+            Uri uri = fromFile(file);
+            imageView.setImageURI(uri);
+//            imageView.setImageURI(Uri.parse("/media/external/images/media/32"));
+        }
+
+        try {
+            Files.walk(getApplication().getDataDir().toPath())
+            .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File file = new File("/storage/emulated/0/Android/data/com.example.democamera/files/Pictures/CameraX-Image");
+
+        ImageView imageView = findViewById(R.id.imageView);
+        String uriString = getApplication().getDataDir().toString() + "/Pictures/37";
+        imageView.setImageURI(Uri.parse(uriString));
+        File dataDir = getApplication().getDataDir();
+        System.out.println(dataDir);
     }
 }
